df942ad9e3c93d20f3c0362cfe26df6ce43a8bd6
CommieKatze <85640544+CommieKatze@users.noreply.github.com>
2022-11-29 00:51:49 -0500

    Update kWSL.cmd

--- kWSL.cmd
+++ kWSL.cmd
@@ -1,7 +1,4 @@
-@ECHO OFF & NET SESSION >NUL 2>&1 
-IF %ERRORLEVEL% == 0 (ECHO Administrator check passed...) ELSE (ECHO You need to run this command with administrative rights.  Is User Account Control enabled? && pause && goto ENDSCRIPT)
-COLOR 1F
-SET GITORG=Avadesktop
+SET GITORG=KDE(ava)console
 SET GITPRJ=kWSL
 SET BRANCH=master
 SET BASE=https://github.com/%GITORG%/%GITPRJ%/raw/%BRANCH%
